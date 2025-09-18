3f714f07861ad2d4030e1c3c6c144df49bb5215d
Luis-Gaspar <gasparjosel350@gmail.com>
2025-09-18 11:37:14 -0600

    subiendo más cambios

--- script.js/wicho.js
+++ script.js/wicho.js
@@ -4,3 +4,8 @@
 console.log(Hola)
 
 //VIVA JOSEFA ORTIZ DE PINEDO
+
+
+
+//a los caballos les dará dolor de Caballo???
+// ´Porque el profe me pide desarrollar algo si yo queria desarrollar un futuro con ella, lo cual ya no es posible?
