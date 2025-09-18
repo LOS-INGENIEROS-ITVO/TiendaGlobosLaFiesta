faf8ef8db6546f994b97052042112325ac9ac38b
Luis-Gaspar <gasparjosel350@gmail.com>
2025-09-18 11:51:43 -0600

    perdidos

--- script.js/wicho.js
+++ script.js/wicho.js
@@ -5,7 +5,9 @@ console.log(Hola)
 
 //VIVA JOSEFA ORTIZ DE PINEDO
 
-
+//////////////////ESTAMOS PERDIDOOOOOOS, PERDIDOS, PERDIDOS.
 
 //a los caballos les dará dolor de Caballo???
 // ´Porque el profe me pide desarrollar algo si yo queria desarrollar un futuro con ella, lo cual ya no es posible?
+
+
