37cd71b1d7786d826811654e644bf23e62fa6252
Erick V.P <termino170@gmail.com>
2025-09-22 13:16:00 -0600

    de nuevo preparacion

---
+++ Erick/rayos.js
@@ -0,0 +1,12 @@
+// a ver que sale
+console.log("Hola, este es el archivo rayos.js");
+//// Otro comentario para probar
+console.log("Probando cambios en el archivo rayos.js");
+// Cambios realizados el 2024-06-10
+console.log("Archivo actualizado el 2024-06-10");
+
+
+
+
+// Fin del archivo rayos.js no me sale
+//22/09/2025
