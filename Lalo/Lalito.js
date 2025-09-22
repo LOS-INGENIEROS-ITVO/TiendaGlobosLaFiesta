bb251e4b87fe19505387b306d99a9792bff76b38
LALO25VG <laloveragal11@gmail.com>
2025-09-21 23:13:31 -0600

    Creacion de mi rama y archivo personal

---
+++ Lalo/Lalito.js
@@ -0,0 +1,5 @@
+
+
+//Yo soy Lalo, un apasionado desarrollador de software con experiencia en la creación de aplicaciones web y móviles. Me especializo en JavaScript, React, Node.js y Python, y disfruto resolviendo problemas complejos a través de código limpio y bien estructurado.
+
+//Mi objetivo es construir soluciones tecnológicas que no solo sean funcionales, sino también intuitivas y agradables para los usuarios. Me encanta aprender nuevas tecnologías y mejorar continuamente mis habilidades para mantenerme al día en este campo en constante evolución.
